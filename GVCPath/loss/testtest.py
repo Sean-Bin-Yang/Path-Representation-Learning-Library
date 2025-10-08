3cc54c258db393dca342688165ea667d2ca8ac6d
Sean Bin Yang <60964780+Sean-Bin-Yang@users.noreply.github.com>
2025-10-08 12:20:22 +0200

    Add files via upload

---
+++ GVCPath/loss/testtest.py
@@ -0,0 +1,11 @@
+import torch
+loss = torch.nn.CrossEntropyLoss()
+input = torch.randn(3, 5, requires_grad=True)
+target = torch.empty(3, dtype=torch.long).random_(5)
+output = loss(input, target)
+output.backward()
+
+input = torch.randn(3, 5, requires_grad=True)
+target = torch.randn(3, 5).softmax(dim=1)
+output = loss(input, target)
+output.backward()
